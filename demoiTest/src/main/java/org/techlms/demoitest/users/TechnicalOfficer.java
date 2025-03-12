28187bec4561296bc715b2957a95f8cfd5d15dae
Dineshs737 <112531821+Dineshs737@users.noreply.github.com>
2025-03-13 00:51:13 +0530

    update user classes

--- demoiTest/src/main/java/org/techlms/demoitest/users/TechnicalOfficer.java
+++ demoiTest/src/main/java/org/techlms/demoitest/users/TechnicalOfficer.java
@@ -1,4 +1,14 @@
 package org.techlms.demoitest.users;
 
-public class TechnicalOfficer {
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+public class TechnicalOfficer extends  User {
+    public TechnicalOfficer(String user_id, String name, String password, String email, String contactNo, String role) {
+        super(user_id, name, password, email, contactNo, "technical_officer");
+    }
 }
--- demoiTest/src/main/java/org/techlms/demoitest/users/User.java
+++ demoiTest/src/main/java/org/techlms/demoitest/users/User.java
@@ -1,4 +1,27 @@
 package org.techlms.demoitest.users;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+@Setter
+@Getter
 public class User {
+    protected String user_id;
+    protected String name;
+    protected String password;
+    protected String email;
+    protected String contactNo;
+    protected String role;
+
+
+    public User(String user_id, String name, String password, String email, String contactNo, String role) {
+        this.user_id = user_id;
+        this.name = name;
+        this.password = password;
+        this.email = email;
+        this.contactNo = contactNo;
+        this.role = role;
+    }
 }
