737a84c79a6953329311cb54925285a2da60f4fa
Dineshs737 <112531821+Dineshs737@users.noreply.github.com>
2025-03-22 20:12:34 +0530

    add login simple ui

--- src/main/java/org/techlms/demoitest/Main.java
+++ src/main/java/org/techlms/demoitest/Main.java
@@ -12,7 +12,8 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        FXMLLoader fxmlLoader = new FXMLLoader(this.getClass().getResource("/org/techlms/demoitest/demo.fxml")) ;
+//        LoginCon
+        FXMLLoader fxmlLoader = new FXMLLoader(this.getClass().getResource("/org/techlms/demoitest/login.fxml")) ;
         Scene scene = new Scene(fxmlLoader.load());
         stage.setScene(scene);
         stage.show();
