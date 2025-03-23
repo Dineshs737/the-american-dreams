6cc143e74e5651c7c369cb0a9583c1d87b762c18
Uthayakumar Dinesh <112531821+Dineshs737@users.noreply.github.com>
2025-03-23 08:49:36 +0530

    Backup (#20)
    * fix error in DBConnection
    
    * add new user table structure
    
    * add login simple ui
    
    * update login.fxml and update style.css
    
    * alert box to system

---
+++ src/main/java/org/techlms/demoitest/ErrorController.java
@@ -0,0 +1,15 @@
+package org.techlms.demoitest;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class ErrorController {
+
+    @FXML
+    private Label errorMessage;
+
+
+
+
+
+}
