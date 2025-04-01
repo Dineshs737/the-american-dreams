a2df4efc42de6faf9f5c5bb041ba7d9b79706698
pemakaran <pemakaran99@gmail.com>
2025-04-01 18:20:10 +0530

    fix error tech officer class

--- src/main/java/org/techlms/demoitest/users/TechnicalOfficer.java
+++ src/main/java/org/techlms/demoitest/users/TechnicalOfficer.java
@@ -1,14 +1,8 @@
 package org.techlms.demoitest.users;
 
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
 
 public class TechnicalOfficer extends  User {
-    public TechnicalOfficer(String user_id, String name, String password, String email, String contactNo, String role) {
-        super(user_id, name, password, email, contactNo, "technical_officer");
+    public TechnicalOfficer(int user_id, String user_name, String name, String password, String email, String contactNo, String role) {
+        super(user_id, user_name, name, password, email, contactNo, "technical officer");
     }
 }
