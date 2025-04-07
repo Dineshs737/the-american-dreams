668d0b2e3f38339fb65182c83d62efc759896b1a
Dineshs737 <dinesh77saarck@gmail.com>
2025-04-07 16:40:59 +0530

    moveeeeeeeeeeeeeeeeeeeeeeeeeeee util java file into package

---
+++ src/main/java/org/techlms/demoitest/util/SeeNoticeBorad.java
@@ -0,0 +1,10 @@
+package org.techlms.demoitest.util;
+
+import org.techlms.demoitest.dto.NoticeBoardDTO;
+
+import java.util.List;
+
+public interface SeeNoticeBorad {
+
+    List<NoticeBoardDTO> getNoticeBoard();
+}
