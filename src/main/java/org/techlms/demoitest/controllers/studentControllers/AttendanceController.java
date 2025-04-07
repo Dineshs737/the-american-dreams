d1f099f88752501c9ad1a7f76d98c64c03070ae0
ANPU 21 <manoranjithananpatasan21@gmial.com>
2025-04-07 18:19:43 +0530

    fix error

--- src/main/java/org/techlms/demoitest/controllers/studentControllers/AttendanceController.java
+++
@@ -1,9 +0,0 @@
-package org.techlms.demoitest.controllers.studentControllers;
-
-import javafx.fxml.FXML;
-import javafx.scene.control.TableView;
-
-public class AttendanceController {
-    @FXML
-    private TableView studentAttendance;
-}
