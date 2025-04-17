3cc79d9c93389093141f60ddcba81ea784eefe72
Pemakaran <165193635+Pemakaran@users.noreply.github.com>
2025-04-18 04:17:58 +0530

    Pemakaran (#101)
    * fix error in User class
    
    * fix error tech officer class
    
    * fix error in student class
    
    * fix error Lecturer class error
    
    * fix error Admin class
    
    * add user SessionManager
    
    * fix error
    
    * update
    
    * update
    
    * New update
    
    * new update
    
    * lecture course dto
    
    * lecture course service add
    
    * lecture image add
    
    * lecture ui add
    
    * database update lecture table and course image data
    
    * update
    
    * update test course images
    
    * update
    
    * update
    
    * update and fix error
    
    * remove old user model
    
    * add Course model
    
    * add User Admin new model
    
    * add new Attendance model
    
    * add new CourseMaterial model
    
    * add new Lecturer model
    
    * add new Student model
    
    * add new Technical Officer Model
    
    * add new Medical Model
    
    * add new User Model
    
    * add new Batch model
    
    * fix error and update it
    
    * remove old User class connection and give new User model connection
    
    * updateeeeeeee and fix it
    
    * update lecturer home page controller
    
    * update lecturer course service
    
    * add lecturer material page controller
    
    * add show students data to lecturer
    
    * add lecturer show student attendance page
    
    * add lecturer  course page service
    
    * add lecturer service page
    
    * add get student interface
    
    * add lecturer course material  ui components
    
    * add lecturer page releted images
    
    * update and fix error
    
    * add lectuer table data course material
    
    * add lecturer id and create new gatter and setter
    
    * update table id
    
    * update course material data fetch method
    
    * git add course table material tableeeee css fileeeeeeeee
    
    * update
    
    ---------
    
    Co-authored-by: Uthayakumar Dinesh <112531821+Dineshs737@users.noreply.github.com>
    Co-authored-by: ANPU 21 <manoranjithananpatasan21@gmial.com>
    Co-authored-by: Dineshs737 <dinesh77saarck@gmail.com>
    Co-authored-by: Thanushka Malshan <thanushkamalshan88@gmail.com>

--- src/main/java/org/techlms/demoitest/controllers/lecturerControllers/CourseManagerialController.java
+++ src/main/java/org/techlms/demoitest/controllers/lecturerControllers/CourseManagerialController.java
@@ -1,38 +1,36 @@
 package org.techlms.demoitest.controllers.lecturerControllers;
 
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.chart.PieChart;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
+import javafx.stage.FileChooser;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import org.techlms.demoitest.model.course.CourseMaterial;
+import org.techlms.demoitest.service.lecturerService.LecturerService;
 
-public class CourseManagerialController {
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.util.ResourceBundle;
+
+public class CourseManagerialController  implements Initializable {
+
+    private final LecturerService service = new LecturerService();
 
     @FXML
     private VBox lecturer;
 
     @FXML
-    private Label totalAdminCount;
-
-    @FXML
-    private Label totalLecturerCount;
-
-    @FXML
-    private Label totalStudentCount;
-
-    @FXML
-    private Label totalTechnicalOfficerCount;
-
-    @FXML
     private VBox userDataCard;
-
-    @FXML
-    private PieChart userPIChart;
 
     @FXML
     private VBox contentContainer;
@@ -52,37 +50,95 @@ public class CourseManagerialController {
 
     }
 
+
+
     @FXML
-    void switchUndergraduatesPage(MouseEvent event) {
+    private TableView<CourseMaterial> courseMaterialTable;
 
-        FXMLLoader fxmlLoader = null;
-        try {
-            try {
-                fxmlLoader = new FXMLLoader(getClass().getResource("/org/techlms/demoitest/lecturer-ui-components/show-all-student-page.fxml"));
-                Parent undergraduatePage = fxmlLoader.load();
+    @FXML
+    private TableColumn<CourseMaterial,Integer> materialNumber;
 
-                // Create a new Stage for the pop-up
-                Stage popUpStage = new Stage();
-                popUpStage.setTitle("Undergraduates Page");
-                popUpStage.initModality(Modality.APPLICATION_MODAL); // Set as a modal window
-                popUpStage.initOwner(contentContainer.getScene().getWindow());
-                popUpStage.setScene(new Scene(undergraduatePage));
+    @FXML
+    private TableColumn<CourseMaterial, CourseMaterial> lectureResource;
 
-                // Set window size (optional)
-                popUpStage.setWidth(800);
-                popUpStage.setHeight(600);
+    @FXML
+    private TableColumn<CourseMaterial, String> lectureTitle;
 
-                // Show the pop-up
-                popUpStage.showAndWait(); // Wait until the pop-up is closed
-                System.out.println("Undergraduates Page Loaded");
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+    @FXML
+    private TableColumn<CourseMaterial, String> lectureUploadDate;
 
-        }catch (Exception e) {
-            e.printStackTrace();
-        }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        materialNumber.setCellValueFactory(cellData->
+                new SimpleIntegerProperty((
+                    cellData.getValue().getCourseMaterialId()
+                )).asObject());
+
+        lectureTitle.setCellValueFactory(cellData->
+                new SimpleStringProperty(
+                        cellData.getValue().getLectureTitle()
+                ));
+
+        lectureUploadDate.setCellValueFactory(cellData->
+                new SimpleStringProperty(
+                        cellData.getValue().getLectureDate()
+                ));
+
+        lectureDownloadResource();
+
+        courseMaterialTable.getItems().setAll(service.getCourseMaterialByCourseCodeAndLecturerId("l0001", "ict2113"));
+
     }
+
+
+    private void lectureDownloadResource(){
+        lectureResource.setCellFactory(
+                col -> new TableCell<>() {
+                    Button downloadButton = new Button("Download");
+                    {
+                        downloadButton.setOnAction(event ->{
+                            System.out.println("Button clicked");
+                            CourseMaterial dto = getTableView().getItems().get(getIndex());
+                            byte[] lectureResource =   dto.getCourseResource();
+                            // open file chooser
+                            FileChooser fileChooser = new FileChooser();
+                            fileChooser.setTitle("Save File");
+                            fileChooser.setInitialFileName(dto.getLectureTitle().trim() + ".pdf");
+
+                            File file = fileChooser.showSaveDialog(getScene().getWindow());
+                            if (file != null) {
+                                try {
+                                    // Save the notice data to the file
+                                    Files.write(file.toPath(), lectureResource);
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+//                                    showError("Failed to save the file.");
+                                    System.out.println("Failed to save the file.");
+                                }
+                            }
+                        });
+
+                        downloadButton.setStyle("-fx-background-color: #2D336B; -fx-text-fill: white; -fx-background-radius: 10px 5px 5px 10px; -fx-font-size: 15px; -fx-cursor: hand;-fx-font-weight: bold");
+                    }
+
+                    protected void updateItem(CourseMaterial item, boolean empty) {
+                        super.updateItem(item, empty);
+                        setGraphic(empty ? null : downloadButton);
+                    }
+
+
+
+
+                });
+
+
+
+
+    }
+
+
 
     @FXML
     void switchUserUpdatePage(MouseEvent event) {
@@ -121,9 +177,43 @@ public class CourseManagerialController {
 
     }
 
+
+    @FXML
+    void switchUndergraduatesPage(MouseEvent event) {
+
+        FXMLLoader fxmlLoader = null;
+        try {
+            try {
+                fxmlLoader = new FXMLLoader(getClass().getResource("/org/techlms/demoitest/lecturer-ui-components/lectuererCourseComponents/show-all-student-page.fxml"));
+                Parent undergraduatePage = fxmlLoader.load();
+
+                // Create a new Stage for the pop-up
+                Stage popUpStage = new Stage();
+                popUpStage.setTitle("Undergraduates Page");
+                popUpStage.initModality(Modality.APPLICATION_MODAL); // Set as a modal window
+                popUpStage.initOwner(contentContainer.getScene().getWindow());
+                popUpStage.setScene(new Scene(undergraduatePage));
+
+                // Set window size (optional)
+                popUpStage.setWidth(800);
+                popUpStage.setHeight(600);
+
+                // Show the pop-up
+                popUpStage.showAndWait(); // Wait until the pop-up is closed
+                System.out.println("Undergraduates Page Loaded");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     @FXML
     void undergaduatesMarks(MouseEvent event) {
 
     }
+
 
 }
