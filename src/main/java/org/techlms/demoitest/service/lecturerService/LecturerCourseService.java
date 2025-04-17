02cf88181f2a347eb43bf1835bb1e9d53d09302c
Dineshs737 <dinesh77saarck@gmail.com>
2025-04-18 02:06:30 +0530

    update lecturer course service

--- src/main/java/org/techlms/demoitest/service/lecturerService/LecturerCourseService.java
+++ src/main/java/org/techlms/demoitest/service/lecturerService/LecturerCourseService.java
@@ -2,6 +2,7 @@ package org.techlms.demoitest.service.lecturerService;
 
 import org.techlms.demoitest.dbconnection.DBConnection;
 import org.techlms.demoitest.dto.lecturerDTO.LecturerCourseDTO;
+import org.techlms.demoitest.model.course.Course;
 import org.techlms.demoitest.util.SessionManager;
 
 import java.sql.Connection;
@@ -12,14 +13,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class LecturerCourseService {
-    public List<LecturerCourseDTO> getAllLecturerCourses() {
+    public List<Course> getAllLecturerCourses() {
         SessionManager user =  SessionManager.getInstance();
-        List<LecturerCourseDTO> lecturerCourses = new ArrayList<>();
+        List<Course> lecturerCourses = new ArrayList<>();
         String sql =
-"""
-SELECT  c.name, c.course_image, c.course_semester FROM course c JOIN lecturing lec ON c.course_code = lec.course_id
-WHERE lec.lecturer_id = ( SELECT l.lecturer_id FROM lecturer l JOIN user u ON l.user_id = u.user_id WHERE u.user_id = ? AND u.role = ?);
- """;
+                """
+                SELECT c.course_code , c.name, c.course_image, c.course_semester FROM course c JOIN lecturing lec ON c.course_code = lec.course_id
+                WHERE lec.lecturer_id = ( SELECT l.lecturer_id FROM lecturer l JOIN user u ON l.user_id = u.user_id WHERE u.user_id = ? AND u.role = ?);
+                 """;
         try{
             Connection con = DBConnection.getConnection();
             PreparedStatement ps = con.prepareStatement(sql);
@@ -28,10 +29,31 @@ WHERE lec.lecturer_id = ( SELECT l.lecturer_id FROM lecturer l JOIN user u ON l.
             ResultSet rs = ps.executeQuery();
             while(rs.next()){
 
-                String courseName = rs.getString(1);
-                byte[] courseImage =rs.getBytes(2);
-                String courseSemester = rs.getString(3);
-                lecturerCourses.add(new LecturerCourseDTO(courseName,courseImage,courseSemester));
+
+//                System.out.println(rs.getString(1));
+
+                String courseCode = rs.getString(1);
+                String courseName = rs.getString(2);
+                byte[] courseImage =rs.getBytes(3);
+                String courseSemester = rs.getString(4);
+
+                /**********
+                 *
+                 *
+                 *
+                 *
+                 *
+                 *      public Course(String courseCode, String courseName, byte[] courseImage, String courseSemester) {
+                 *         this.courseCode = courseCode;
+                 *         this.courseName = courseName;
+                 *         this.courseImage = courseImage;
+                 *         this.courseSemester = courseSemester;
+                 *     }
+                 *
+                 *
+                 */
+
+                lecturerCourses.add(new Course(courseCode,courseName,courseImage,courseSemester));
 
             }
         } catch (SQLException e) {
